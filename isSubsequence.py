1fa4a7a4ec35c01efd0becd81e0c1fea110c8c3c
hasan zaidi <hasanammarza@usf.edu>
2024-02-20 16:05:19 -0500

    almost working isSubsequence

--- isSubsequence.py
+++ isSubsequence.py
@@ -1,37 +1,28 @@
 
-def isSubsequence( s, t):
+def isSubsequence(t, s):
 
-  SList = []
-  TList = []
-  
-  for itemS in s:
-    SList.append(itemS)
-  for itemT in t:
-    TList.append(itemT)
-  
-  print()
-  print(TList)
-  print(SList)
-  print()
+  newList = []
 
-  
-  for alphS in SList:
-    for alphT in TList:
-      if alphS == alphT:
-        continue
-      else:
-        TList.remove(alphT)
-    # SList.remove(alphS)
-        
+  for letter_in_t in t:
+    for letter_in_s in s:
+      if letter_in_s == letter_in_t:
+        newList.append(letter_in_s)
+        s = chop_array_until_item(s,letter_in_s) 
 
-  print(TList)
-  
-  given_s = list(s)
-  print(given_s)
-  return(TList==given_s)
+  #compare s with updated t.list
+  if list(t) == newList:
+    return True
+  else:
+    return False
 
 
+def chop_array_until_item(arr, item):
+    if item in arr:
+        index = arr.index(item)  # Find the index of the item
+        chopped_array = arr[index+1:]  # Slice the array until that index
+        return chopped_array
+    else:
+        return arr  # If the item is not found, return the original array
 
 
-
-
+      
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import isSubsequence as iS
 import removeDuplicates as rD
 
-print(iS.isSubsequence("abc", "ahbgdc"))
-print(iS.isSubsequence("axc", "ahbgdc"))
-print(iS.isSubsequence("ab", "baab")) # Should be true
-print(iS.isSubsequence("abc", "abc")) # Should be true
+# print(iS.isSubsequence("abc","ahbgdc"))
+# print(iS.isSubsequence("axc","ahbgdc"))
+print(iS.isSubsequence("ab","baab")) # Should be true
+print(iS.isSubsequence("abc","abc")) # Should be true
 
 # print(rD.removeDuplicates([1,1,2222,2,2.2,2]))
 # print(rD.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
