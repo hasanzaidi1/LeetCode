12da733a581b08acfdddda56448fd07e0937cc2d
hasanzaidi1 <hasanammarza@usf.edu>
2024-06-24 14:27:55 -0400

    working

--- maxProduct.py
+++ maxProduct.py
@@ -1,23 +1,45 @@
 
 def maxProduct(words):
-    maxWord = findMaxCntChrs(words)
-    maxProd = 0
-    for word in words:
-        if word != maxWord and len(word) == len(maxWord): 
-            maxProd = len(word) * len(maxWord)
-        else:
-            words.remove(maxWord)
-            maxWord = findMaxCntChrs(words)
-    return maxProd
+    maxWords = find2MaxCntWrds(words)
+    return len(maxWords[0]) * len(maxWords[1])
 
 
-def findMaxCntChrs(words):
-    currWrd = ''
+def find2MaxCntWrds(words):
+    mxCtWrd2 = ''
+
+    while mxCtWrd2 == '' and len(words) != 0:
+
+        mxCtWrd1 = findMax(words)
+        words.remove(mxCtWrd1)
+        
+        for word in words:
+            if len(word) == len(mxCtWrd1) and noCommonLetters(word,mxCtWrd1):     # maybe add into cond that word isnt the same
+                mxCtWrd2 = word
+                break
+            else:
+                continue
+
+    return mxCtWrd1, mxCtWrd2
+
+
+def findMax(words):
+
     mxCtWrd = words[0]
     for i in range(len(words)):
         currWrd = words[i]
         if len(currWrd) > len(mxCtWrd):
             mxCtWrd = currWrd
-        else:
-            continue
     return mxCtWrd
+
+
+
+def noCommonLetters(word1,word2):
+
+    # Convert both words to sets of characters
+    set1 = set(word1)
+    set2 = set(word2)
+
+    # Check for intersection between the sets
+    if set1 & set2:
+        return False
+    return True
