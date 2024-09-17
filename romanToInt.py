524475a20717acf25a1f592e690194b24b5059b8
hasanzaidi1 <hasanammarza@usf.edu>
2024-09-17 16:19:19 -0400

    wokring with linear

--- romanToInt.py
+++ romanToInt.py
@@ -11,5 +11,28 @@ def romanToInt(s):
         'M':             1000
     }
 
-    return 
+    sum = 0
+    s_list = list(s)
+
+    for i in range(len(s_list)):
+        pair = False
+        if i > 0:
+            # check if i lesser than i-1, if not then take them as a pair
+            curr = hashTable[s_list[i]]
+            prev = hashTable[s_list[i-1]]
+            pair = curr > prev
+            # print("curr Val :",hashTable[s_list[i]])
+            # print("prev Val :",hashTable[s_list[i-1]])
+            # print("SUM: ", sum)
+
+        if pair:
+            sum += hashTable[s_list[i-1]]-hashTable[s_list[i]]
+        else:
+            sum += hashTable[s_list[i]]
+    return sum
+
+
+print(romanToInt("LVIII"))
+print(romanToInt("III"))
+print(romanToInt("MCMXCIV"))
 
